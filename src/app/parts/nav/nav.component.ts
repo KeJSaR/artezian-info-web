646969282401729783fa1ad2d722645a981c079e
Constantine Verenich <verenich@gmail.com>
2017-09-15 15:42:44 +0300

    Refactored nav component

--- src/app/parts/nav/nav.component.ts
+++ src/app/parts/nav/nav.component.ts
@@ -2,16 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
-  template: `
-    <nav>
-      <a routerLink="/info">Об ААЭ</a>
-      <a routerLink="/articles">Статьи</a>
-      <a routerLink="/gallery">Фото</a>
-      <a routerLink="/rules">Информация</a>
-      <a routerLink="/contacts">Контакты</a>
-    </nav>
-  `,
-  styles: []
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.sass']
 })
 export class NavComponent implements OnInit {
 
