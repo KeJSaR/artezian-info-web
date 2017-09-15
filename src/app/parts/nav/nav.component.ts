14632d8132b9083e08d33e3aed7e15713ce24b1d
Constantine Verenich <verenich@gmail.com>
2017-09-15 12:46:05 +0300

    Changed main nav language

--- src/app/parts/nav/nav.component.ts
+++ src/app/parts/nav/nav.component.ts
@@ -4,12 +4,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-nav',
   template: `
     <nav>
-      <a routerLink="/info">Info</a>
-      <a routerLink="/articles">Articles</a>
-      <a routerLink="/gallery">Gallery</a>
-      <a routerLink="/rules">Rules</a>
-      <a routerLink="/application">Application</a>
-      <a routerLink="/contacts">Contacts</a>
+      <a routerLink="/info">Об ААЭ</a>
+      <a routerLink="/articles">Статьи</a>
+      <a routerLink="/gallery">Фото</a>
+      <a routerLink="/rules">Информация</a>
+      <a routerLink="/application">Заявка</a>
+      <a routerLink="/contacts">Контакты</a>
     </nav>
   `,
   styles: []
