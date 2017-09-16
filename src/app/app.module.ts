fb8cbe8e4fd2b7e22399833a27c5443e063d49da
Constantine Verenich <verenich@gmail.com>
2017-09-16 12:31:53 +0300

    Aligned import statements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }           from '@angular/platform-browser';
+import { NgModule }                from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent }            from './app.component';
+import { AppRoutingModule }        from './app-routing.module';
 
-import { ApplicationComponent } from './pages/application/application.component';
-import { ArticleComponent } from './pages/article/article.component';
-import { ArticlesComponent } from './pages/articles/articles.component';
-import { ContactsComponent } from './pages/contacts/contacts.component';
-import { GalleryComponent } from './pages/gallery/gallery.component';
-import { InfoComponent } from './pages/info/info.component';
-import { IntroComponent } from './pages/intro/intro.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { RulesComponent } from './pages/rules/rules.component';
+import { ApplicationComponent }    from './pages/application/application.component';
+import { ArticleComponent }        from './pages/article/article.component';
+import { ArticlesComponent }       from './pages/articles/articles.component';
+import { ContactsComponent }       from './pages/contacts/contacts.component';
+import { GalleryComponent }        from './pages/gallery/gallery.component';
+import { InfoComponent }           from './pages/info/info.component';
+import { IntroComponent }          from './pages/intro/intro.component';
+import { NotFoundComponent }       from './pages/not-found/not-found.component';
+import { RulesComponent }          from './pages/rules/rules.component';
 
 import { ArticleContentComponent } from './parts/article-content/article-content.component';
-import { ArticlesListComponent } from './parts/articles-list/articles-list.component';
-import { FooterComponent } from './parts/footer/footer.component';
-import { HeaderComponent } from './parts/header/header.component';
-import { NavComponent } from './parts/nav/nav.component';
-import { SocialComponent } from './parts/social/social.component';
+import { ArticlesListComponent }   from './parts/articles-list/articles-list.component';
+import { FooterComponent }         from './parts/footer/footer.component';
+import { HeaderComponent }         from './parts/header/header.component';
+import { NavComponent }            from './parts/nav/nav.component';
+import { SocialComponent }         from './parts/social/social.component';
 
-import { GetDataService } from './services/get-data.service';
+import { GetDataService }          from './services/get-data.service';
 
 @NgModule({
   declarations: [
