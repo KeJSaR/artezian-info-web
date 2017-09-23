184f170b4f0d24b16b9bc11d2b37931de594e08c
Constantine Verenich <verenich@gmail.com>
2017-09-24 02:55:01 +0300

    Updated add process in admin scripts

--- src/assets/admin-scripts/add.js
+++ src/assets/admin-scripts/add.js
@@ -1,5 +1,5 @@
 
-// Send new article data on server #####################################
+// Create empty article in DB to get article_id ########################
 
 function getCurrUrl() {
   return window.location.href.replace('add.html', '');
@@ -9,46 +9,64 @@ function getCurrDate() {
   return new Date().toISOString().slice(0, 19).replace('T', ' ');
 }
 
-function modalBox(headerText, bodyText, footerText) {
-  $('#modal').css('display', 'block');
-
-  $('#modal .close').click(function() {
-    $('#modal').css('display', 'none');
-  });
-
-  $('#modal .modal-header').prepend($('<h2></h2>').html(headerText));
-  $('#modal .modal-body').append($('<div></div>').html(bodyText));
-  $('#modal .modal-footer').append($('<h3></h3>').html(footerText));
-}
-
-$('#submit').click(function() {
+function sendArticleData(url, id) {
 
   tinyMCE.triggerSave();
 
-  var currUrl = getCurrUrl();
-  var url = currUrl + 'add.php';
+  let articleSection = $('#article-section input[name=section]:checked').data('section');
+  let articleTitle   = $('#article-title').val();
+  let articleIntro   = $('#article-intro').val();
+  let articleContent = $('#article-content').val();
 
-  var currDate = getCurrDate();
+  var fd = new FormData();
+  var file = $('#article-image')[0].files[0];
+  fd.append('file', file);
 
-  $.post(url, {
-    'title':   $('#article-title').val(),
-    'intro':   $('#article-intro').val(),
-    'section': $('#article-section').val(),
-    'image':   $('#article-image').val(),
-    'content': $('#article-content').val(),
-    'date':    currDate
-  }).done(function( data ) {
+  $.post(url, fd, {
+    'update'  : 1,
+    'id'      : id,
+    'section' : articleSection,
+    'title'   : articleTitle,
+    'intro'   : articleIntro,
+    'content' : articleContent
+  }).done(function(data) {
     let h = 'Статья отправлена на сервер';
     let b = data;
-    let f = 'Just footer text';
-    modalBox(h, b, f);
+    $('#test-data').html('<div>' + h + '</div><div>' + b + '</div>');
   }).fail(function(e) {
     let h = 'Произошла ошибка. Статья не добавлена';
     let b = e.responseText;
-    let f = 'Just footer text';
-    modalBox(h, b, f);
+    $('#test-data').html('<div>' + h + '</div><div>' + b + '</div>');
   });
-});
+}
+
+function getArticleId() {
+
+    var currUrl  = getCurrUrl();
+    var url      = currUrl + 'add.php';
+
+    var currDate = getCurrDate();
+
+    $.post(url, {
+      'update' : 0,
+      'date'   : currDate
+    }).done(function(id) {
+      $('#test-data').html('<div>Article ID: ' + id + '</div>');
+      $('body').data('article-id', id);
+
+      // On submit - send article data to the server
+      $('#submit').click(function() {
+        sendArticleData(url, id);
+      });
+
+    }).fail(function(e) {
+      let h = 'Произошла ошибка. Article ID не получено.';
+      let b = e.responseText;
+      $('#test-data').html('<div>' + h + '</div><div>' + b + '</div>');
+    });
+}
+
+getArticleId();
 
 // TinyMCE #############################################################
 
@@ -60,26 +78,25 @@ $(document).on('focusin', function(e) {
 });
 
 tinymce.init({
-  selector: '#article-intro',
-  height: 294,
-  menubar: false,
-  statusbar: false,
-  paste_as_text: true,
-  plugins: 'paste autolink advcode link lists textcolor colorpicker',
-  toolbar: 'bold italic underline | subscript superscript | strikethrough forecolor backcolor | removeformat | bullist numlist | link unlink | code'
+  selector      : '#article-intro',
+  menubar       : false,
+  statusbar     : false,
+  paste_as_text : true,
+  plugins       : 'paste autolink autoresize advcode link lists textcolor colorpicker',
+  toolbar       : 'bold italic underline | subscript superscript | strikethrough forecolor backcolor | removeformat | bullist numlist | link unlink | code'
 });
 
 tinymce.init({
-  selector: '#article-content',
-  menubar: false,
-  statusbar: false,
-  paste_as_text: true,
-  image_advtab: true,
-  plugins: 'autolink autoresize hr lists link image charmap paste searchreplace visualblocks visualchars advcode insertdatetime media table imagetools directionality textcolor',
-  toolbar1: 'alignleft aligncenter alignright alignjustify alignnone | outdent indent | ltr rtl | styleselect formatselect fontsizeselect',
-  toolbar2: 'bullist numlist | bold italic underline subscript superscript | strikethrough forecolor backcolor | removeformat | blockquote hr | link unlink | searchreplace | code',
-  toolbar3: 'image rotateleft rotateright flipv fliph editimage imageoptions | media charmap insertdatetime | cut copy paste | table | visualblocks visualchars | undo redo',
-  table_toolbar: 'tabledelete tablecellprops tablemergecells tablesplitcells tableinsertrowbefore tableinsertrowafter tabledeleterow tablerowprops tablecutrow tablecopyrow tablepasterowbefore tablepasterowafter tableinsertcolbefore tableinsertcolafter tabledeletecol'
+  selector      : '#article-content',
+  menubar       : false,
+  statusbar     : false,
+  paste_as_text : true,
+  image_advtab  : true,
+  plugins       : 'autolink autoresize hr lists link image charmap paste searchreplace visualblocks visualchars advcode insertdatetime media table imagetools directionality textcolor',
+  toolbar1      : 'alignleft aligncenter alignright alignjustify alignnone | outdent indent | ltr rtl | styleselect formatselect fontsizeselect',
+  toolbar2      : 'bullist numlist | bold italic underline subscript superscript | strikethrough forecolor backcolor | removeformat | blockquote hr | link unlink | searchreplace | code',
+  toolbar3      : 'image rotateleft rotateright flipv fliph editimage imageoptions | media charmap insertdatetime | cut copy paste | table | visualblocks visualchars | undo redo',
+  table_toolbar : 'tabledelete tablecellprops tablemergecells tablesplitcells tableinsertrowbefore tableinsertrowafter tabledeleterow tablerowprops tablecutrow tablecopyrow tablepasterowbefore tablepasterowafter tableinsertcolbefore tableinsertcolafter tabledeletecol'
 });
 
 // Preview an image before it is uploaded ##############################
@@ -94,6 +111,8 @@ function readURL(input) {
     }
 
     reader.readAsDataURL(input.files[0]);
+
+    $('#sample-image').show();
   }
 }
 
--- src/assets/admin-scripts/add.php
+++ src/assets/admin-scripts/add.php
@@ -8,36 +8,50 @@ function connect()
   return $db->getConnection();
 }
 
-function get_post_data()
+function check_post_type()
+{
+  return filter_input(INPUT_POST, "update", FILTER_SANITIZE_NUMBER_INT);
+}
+
+function get_article_update_data()
 {
   return array(
+    "section" => $_POST["section"],
     "title"   => $_POST["title"],
     "intro"   => $_POST["intro"],
-    "section" => $_POST["section"],
-    "image"   => $_POST["image"],
     "content" => $_POST["content"],
-    "date"    => $_POST["date"],
+    "id"      => $_POST["id"],
   );
 }
 
-function get_menu_data($section, $date, $image, $title, $intro, $content)
+function get_article_initial_data()
+{
+  return array(
+    "date" => $_POST["date"],
+  );
+}
+
+function update_article($article_data)
 {
   $dbh = connect();
 
-  $sql = "INSERT INTO article (section_id, date, image, title, intro, content)
-                 VALUES (:section, :date, :image, :title, :intro, :content)";
+  $sql = "UPDATE article
+          SET section_id = :section,
+              title      = :title,
+              intro      = :intro,
+              content    = :content
+          WHERE article_id = :article_id";
 
   try {
     $sth = $dbh->prepare($sql);
     $sth->execute(array(
-      ':section' => $section,
-      ':date'    => $date,
-      ':image'   => $image,
-      ':title'   => $title,
-      ':intro'   => $intro,
-      ':content' => $content
+      ':section'    => $article_data["section"],
+      ":title"      => $article_data["title"],
+      ":intro"      => $article_data["intro"],
+      ":content"    => $article_data["content"],
+      ":article_id" => $article_data["id"],
     ));
-    return "Статья успешно добавлена!";
+    return false;
   }
   catch(PDOException $e)
   {
@@ -45,15 +59,40 @@ function get_menu_data($section, $date, $image, $title, $intro, $content)
   }
 }
 
-$post_data = get_post_data();
+function create_article($article_data)
+{
+  $dbh = connect();
 
-$result = get_menu_data(
-  $post_data['section'],
-  $post_data['date'],
-  $post_data['image'],
-  $post_data['title'],
-  $post_data['intro'],
-  $post_data['content']
-);
+  $sql = "INSERT INTO article (date)
+          VALUES (:date)";
 
-echo $result;
+  try {
+    $sth = $dbh->prepare($sql);
+    $sth->execute(array(
+      ':date' => $article_data["date"],
+    ));
+    return $dbh->lastInsertId();
+  }
+  catch(PDOException $e)
+  {
+    return $sql . "<br>" . $e->getMessage();
+  }
+}
+
+function init() {
+  // Check POST type
+  $is_update = check_post_type();
+
+  if ($is_update) {
+    // Get article data from POST
+    $article_data = get_article_update_data();
+    // Update exist article with data
+    return update_article($article_data);
+  } else {
+    // Create empty article to get its ID
+    $article_data = get_article_initial_data();
+    return create_article($article_data);
+  }
+}
+
+echo init();
