f61aba6c79fb7016a5d5c08c412e8ed5fd2ce89d
Constantine Verenich <verenich@gmail.com>
2017-09-29 01:18:07 +0300

    Temp files for image uploading process

--- src/assets/admin-scripts/img-upload/script.js
+++ src/assets/admin-scripts/img-upload/script.js
@@ -15,38 +15,81 @@ $(function() {
 
 });
 
-function fileUpload(event) {
+function dataURItoBlob(dataURI) {
 
-  //notify user about the file upload status
-  $('#dropBox').html(event.target.value + ' uploading...');
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
 
-  //get selected file
-  files = event.target.files;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+    byteString = atob(dataURI.split(',')[1]);
+  } else {
+    byteString = unescape(dataURI.split(',')[1]);
+  }
 
-  //form data check the above bullet for what it is
-  var data = new FormData();
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
-  //file data is presented as an array
-  for (var i = 0; i < files.length; i++) {
-    var file = files[i];
-    if (!file.type.match('image.*')) {
-      //check file type
-      $('#dropBox').html('Please choose an images file.');
-    } else if (file.size > 1048576) {
-      //check file size (in bytes)
-      $('#dropBox').html('Sorry, your file is too large (>1 MB)');
-    } else {
-      //append the uploadable file to FormData object
-      data.append('file', file, file.name);
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
 
-      //create a new XMLHttpRequest
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], {type:mimeString});
+}
+
+function fileUpload(e) {
+
+  $('#dropBox').html(e.target.value + ' uploading...');
+
+  var file = e.target.files[0];
+
+  if (!file.type.match('image.*')) {
+    $('#dropBox').html('Please choose an images file.');
+  } else {
+
+    var img = document.createElement("img");
+    img.src = window.URL.createObjectURL(file);
+    $('#thumbnail').html(img);
+
+    img.onload = function() {
+      // var canvas = document.getElementById('canvas');
+      var canvas = document.createElement("canvas");
+      var MAX_WIDTH = 300;
+      var MAX_HEIGHT = 300;
+      var width = img.width;
+      var height = img.height;
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
+      }
+      canvas.width = width;
+      canvas.height = height;
+      var ctx = canvas.getContext("2d");
+      // manually draw a white background on the canvas
+      ctx.fillStyle = '#fff';
+      ctx.fillRect(0, 0, width, height);
+      // draw the image
+      ctx.drawImage(img, 0, 0, width, height);
+
+      var dataURL = canvas.toDataURL('image/jpeg', 0.9);
+      var blob = dataURItoBlob(dataURL);
+      var fd = new FormData();
+      fd.append('file', blob, 'blob.jpeg');
+
       var xhr = new XMLHttpRequest();
-
-      //post file data for upload
       xhr.open('POST', 'upload.php', true);
-      xhr.send(data);
+      xhr.send(fd);
       xhr.onload = function () {
-        //get response and show the uploading status
+        $('#dropBox').html(xhr.responseText);
         var response = JSON.parse(xhr.responseText);
         if (xhr.status === 200 && response.status == 'ok') {
           $('#dropBox').html('File has been uploaded successfully. Click to upload another.');
@@ -56,7 +99,6 @@ function fileUpload(event) {
           $('#dropBox').html('Some problem occured, please try again.');
         }
       };
-    }
+    };
   }
-
 }
