0a0f628a0a3be449460e49f04944f89500d38d88
Constantine Verenich <verenich@gmail.com>
2017-10-27 02:15:47 +0300

    Add base navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }              from '@angular/core';
+import { Routes, RouterModule }  from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent }     from './pages/not-found/not-found.component';
+import { PageAboutComponent }    from './pages/page-about/page-about.component';
+import { PageArticlesComponent } from './pages/page-articles/page-articles.component';
+import { PageGalleryComponent }  from './pages/page-gallery/page-gallery.component';
+import { PageRulesComponent }    from './pages/page-rules/page-rules.component';
+import { PageFormComponent }     from './pages/page-form/page-form.component';
+
+const routes: Routes = [
+  { path: '',             redirectTo: '/about', pathMatch: 'full' },
+  { path: 'about',        component: PageAboutComponent },
+  { path: 'articles',     component: PageArticlesComponent },
+  { path: 'articles/:id', component: PageArticlesComponent },
+  { path: 'gallery',      component: PageGalleryComponent },
+  { path: 'gallery/:id',  component: PageGalleryComponent },
+  { path: 'rules',        component: PageRulesComponent },
+  { path: 'rules/:id',    component: PageRulesComponent },
+  { path: 'form',         component: PageFormComponent },
+  { path: '**',           component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
