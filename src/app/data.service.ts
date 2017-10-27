b1ff1b71e5358cef21162f89f5ab3e6b25b4f574
Constantine Verenich <verenich@gmail.com>
2017-10-27 22:15:49 +0300

    Add get menu data to data servise

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,37 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable }  from '@angular/core';
+import { HttpClient }  from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
+import { Observable }  from 'rxjs/Observable';
 
-interface ItemsResponse {
-  name: string;
-  string: string;
-}
+import 'rxjs/add/operator/map';
+
+import { MenuItem } from './interfaces/menu-item';
 
 @Injectable()
 export class DataService {
-
+  
   url = 'http://localhost/artezian-info-web/src/assets/data.php';
-  usn: string = 'myusernamemyusername';
-  psd: string = 'mypasswordmypassword';
-  body: string = `username=${this.usn}&password=${this.psd}`;
+  // req: string = `username=${this.usn}&password=${this.psd}`;
+  req: string = 'get=menu';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  getData() {
-
-    const req = this.http.post<ItemsResponse>
-      (this.url, this.body, {
+  getMenu(): Observable<MenuItem[]> {
+    return this.http.post(this.url, this.req, {
         headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
-      })
-        .subscribe(
-          res => {
-            console.log(res.name + ' ' + res.string);
-          },
-          err => {
-            console.log(err);
-          }
-        );
-
+      }).map((resp: MenuItem[]) => {
+        let menu: MenuItem[] = resp;
+        return menu;
+      });
   }
 
 }
