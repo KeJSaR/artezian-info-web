509b9e9c4c72cac861de33846ac6ec404c85587e
Constantine Verenich <verenich@gmail.com>
2017-10-27 22:28:36 +0300

    Refactor dataservice

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,24 +5,20 @@ import { Observable }  from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
 
-import { MenuItem } from './interfaces/menu-item';
+import { MenuItem } from './interfaces/menu-item.interface';
 
 @Injectable()
 export class DataService {
   
   url = 'http://localhost/artezian-info-web/src/assets/data.php';
   // req: string = `username=${this.usn}&password=${this.psd}`;
-  req: string = 'get=menu';
 
   constructor(private http: HttpClient) { }
-
+  
   getMenu(): Observable<MenuItem[]> {
-    return this.http.post(this.url, this.req, {
-        headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
-      }).map((resp: MenuItem[]) => {
-        let menu: MenuItem[] = resp;
-        return menu;
-      });
+    return this.http.post(this.url, 'get=menu', {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    }).map((resp: MenuItem[]) => resp);
   }
 
 }
