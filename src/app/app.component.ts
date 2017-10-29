0bebf6fddf5fc516e1892134b9ef7b0326cb35eb
Constantine Verenich <verenich@gmail.com>
2017-10-29 22:18:38 +0300

    Add menu to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DataService }       from './data.service';
+import { Menu }              from './interfaces/menu.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  
+  private menu: Menu[];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getMenu().subscribe((data) => this.menu = data);
+  }
+  
+}
