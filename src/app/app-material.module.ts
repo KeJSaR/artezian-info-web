f6bb0891c8d29824de6aeb55fc3d3c812af31c1c
Constantine Verenich <verenich@gmail.com>
2017-10-29 16:54:51 +0300

    Add Angular Material

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule }          from '@angular/core';
+import { MatButtonModule }   from '@angular/material';
+import { MatCheckboxModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule, 
+    MatCheckboxModule
+  ],
+  exports: [
+    MatButtonModule, 
+    MatCheckboxModule
+  ],
+})
+export class AppMaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
-import { BrowserModule }         from '@angular/platform-browser';
-import { NgModule }              from '@angular/core';
-import { HttpClientModule }      from '@angular/common/http';
+import { BrowserModule }           from '@angular/platform-browser';
+import { NgModule }                from '@angular/core';
+import { HttpClientModule }        from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule }      from './app-routing.module';
-import { AppComponent }          from './app.component';
+import { AppRoutingModule }        from './app-routing.module';
+import { AppMaterialModule }       from './app-material.module';
 
-import { DataService }           from './data.service';
-import { PagesComponent }        from './pages/pages.component';
+import { DataService }             from './data.service';
 
-import { HeaderComponent }       from './parts/header/header.component';
-import { FooterComponent }       from './parts/footer/footer.component';
-import { MenuComponent }         from './parts/menu/menu.component';
-import { TextComponent }         from './parts/text/text.component';
-import { IntroComponent }        from './parts/intro/intro.component';
-import { ArticleComponent }      from './parts/article/article.component';
-import { BlogComponent }         from './parts/blog/blog.component';
-import { ContentComponent }      from './parts/content/content.component';
-import { NotFoundComponent }     from './parts/not-found/not-found.component';
-import { GalleryComponent } from './parts/gallery/gallery.component';
-import { AlbumComponent } from './parts/album/album.component';
-import { FormComponent } from './parts/form/form.component';
+import { AppComponent }            from './app.component';
+import { PagesComponent }          from './pages/pages.component';
+
+import { HeaderComponent }         from './parts/header/header.component';
+import { FooterComponent }         from './parts/footer/footer.component';
+import { MenuComponent }           from './parts/menu/menu.component';
+import { TextComponent }           from './parts/text/text.component';
+import { IntroComponent }          from './parts/intro/intro.component';
+import { ArticleComponent }        from './parts/article/article.component';
+import { BlogComponent }           from './parts/blog/blog.component';
+import { ContentComponent }        from './parts/content/content.component';
+import { NotFoundComponent }       from './parts/not-found/not-found.component';
+import { GalleryComponent }        from './parts/gallery/gallery.component';
+import { AlbumComponent }          from './parts/album/album.component';
+import { FormComponent }           from './parts/form/form.component';
 
 @NgModule({
   declarations: [
@@ -41,9 +44,15 @@ import { FormComponent } from './parts/form/form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
-  providers: [DataService],
-  bootstrap: [AppComponent]
+  providers: [
+    DataService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
