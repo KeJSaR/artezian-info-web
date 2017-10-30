23636b92d9bf6606d2afdd2d5ac110979d24c429
Constantine Verenich <verenich@gmail.com>
2017-10-30 04:07:07 +0300

    Add functions to page service

--- src/app/services/page.service.ts
+++ src/app/services/page.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import { Page }       from '../interfaces/page.interface';
+
 @Injectable()
 export class PageService {
 
+  private dict: Page = {
+    page: 'main'
+  }
+
   constructor() { }
+
+  setPage(page): void {
+    this.dict.page = page;
+  }
+
+  getPage(): string {
+    return this.dict.page;
+  }
 
 }
