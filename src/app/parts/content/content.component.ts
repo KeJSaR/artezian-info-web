1c7096c1ad74282b8f04745b95b24d68da23e95d
Constantine Verenich <verenich@gmail.com>
2017-11-01 02:17:56 +0300

    Add basic functionality to content component

--- src/app/parts/content/content.component.ts
+++ src/app/parts/content/content.component.ts
@@ -16,14 +16,38 @@ export class ContentComponent implements OnInit {
 
   @Input() page: string;
 
-  private texts  : Text[];
-  private intros : Intro[];
+  private texts   : Text[];
+  private intros  : Intro[];
+  private isReady : boolean = false;
+  private content = [];
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.getText(this.page).subscribe((data) => this.texts = data);
-    this.dataService.getIntro(this.page).subscribe((data) => this.intros = data);
+
+    this.dataService.getText(this.page).subscribe((data) => {
+      this.texts = data;
+      this.makeContent();
+    });
+
+    this.dataService.getIntro(this.page).subscribe((data) => {
+      this.intros = data;
+      this.makeContent();
+    });
+
+  }
+
+  private makeContent(): void {
+    if (this.texts !== undefined && this.intros !== undefined) {
+      let tL = this.texts.length;
+      let iL = this.intros.length;
+      let max = tL > iL ? tL : iL;
+      for (let i = 0; i < max; i++) {
+        if (i < tL) this.content.push({type: 'text',  data: this.texts[i]});
+        if (i < iL) this.content.push({type: 'intro', data: this.intros[i]});
+      }
+      if (max > 0) this.isReady = true;
+    }
   }
 
 }
