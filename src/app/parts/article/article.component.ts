9b4c32859e960f0a229271376ae66b41ee5a26d9
Constantine Verenich <verenich@gmail.com>
2017-11-01 02:31:33 +0300

    Add base functionality for article component

--- src/app/parts/article/article.component.ts
+++ src/app/parts/article/article.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { Input }     from '@angular/core';
+import { Component }   from '@angular/core';
+import { OnInit }      from '@angular/core';
+import { Input }       from '@angular/core';
+
+import { DataService } from '../../services/data.service';
+
+import { Article }     from '../../interfaces/article.interface';
 
 @Component({
   selector    : 'app-article',
   templateUrl : './article.component.html',
   styleUrls   : ['./article.component.sass']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
 
   @Input() page  : string;
   @Input() alias : string;
+
+  private article: Article;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+
+    this.dataService.getArticle(this.page, this.alias).subscribe((data) => {
+      this.article = data;
+    });
+
+  }
 
 }
