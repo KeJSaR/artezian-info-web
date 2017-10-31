52e3a0e4092c34bfe82e4018b9b6cc7261947916
Constantine Verenich <verenich@gmail.com>
2017-10-31 05:07:55 +0300

    Add get text and intro data in content component

--- src/app/parts/content/content.component.ts
+++ src/app/parts/content/content.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { Input }     from '@angular/core';
+import { Component }   from '@angular/core';
+import { OnInit }      from '@angular/core';
+import { Input }       from '@angular/core';
+
+import { DataService } from '../../services/data.service';
+
+import { Text }        from '../../interfaces/text.interface';
+import { Intro }       from '../../interfaces/intro.interface';
 
 @Component({
   selector    : 'app-content',
   templateUrl : './content.component.html',
   styleUrls   : ['./content.component.sass']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
 
   @Input() page: string;
+
+  private texts  : Text[];
+  private intros : Intro[];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getText(this.page).subscribe((data) => this.texts = data);
+    this.dataService.getIntro(this.page).subscribe((data) => this.intros = data);
+  }
 
 }
