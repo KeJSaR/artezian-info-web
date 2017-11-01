2dd40cbd1befc14bf40ef983b3328d2497cce78c
Constantine Verenich <verenich@gmail.com>
2017-11-01 19:41:40 +0300

    Refactor page component and add title property

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -6,34 +6,58 @@ import { DataService }    from '../services/data.service';
 import { Menu }           from '../interfaces/menu.interface';
 
 @Component({
-  selector: 'app-page',
-  templateUrl: './page.component.html',
-  styleUrls: ['./page.component.sass']
+  selector    : 'app-page',
+  templateUrl : './page.component.html',
+  styleUrls   : ['./page.component.sass']
 })
 export class PageComponent implements OnInit {
-  
-  private menu:  Menu[];
-  private page:  string = '';
-  private alias: string = '';
-  private type:  string = '';
+
+  private menu  : Menu[];
+  private page  : string = '';
+  private title : string = '';
+  private alias : string = '';
+  private type  : string = '';
 
   constructor(
-    private route: ActivatedRoute,
-    private dataService: DataService
+    private route       : ActivatedRoute,
+    private dataService : DataService
   ) {
 
-    this.page = route.snapshot.data.page;
-    this.type = route.snapshot.data.page;
+    let isAlias: boolean    = this.checkAlias();
+    if (isAlias) this.alias = this.getAlias();
 
-    if (route.snapshot.url.length > 1) {
-      this.alias = route.snapshot.params.alias;
-      this.type += '+';
-    }
+    this.page = this.getPage();
+    this.type = this.getType(isAlias);
 
   }
-  
+
   ngOnInit() {
-    this.dataService.getMenu().subscribe((data) => this.menu = data);
+    return this.dataService.getMenu().subscribe((data) => {
+      this.menu  = data;
+      this.title = this.getTitle();
+    });
+  }
+
+  private getPage(): string {
+    return this.route.snapshot.data.page;
+  }
+
+  private checkAlias(): boolean {
+    return this.route.snapshot.url.length > 1
+  }
+
+  private getAlias(): string {
+    return this.route.snapshot.params.alias;
+  }
+
+  private getType(isAlias): string {
+    return isAlias ? this.page + '+' : this.page;
+  }
+
+  private getTitle(): string {
+    for (let item of this.menu) {
+      if (item.alias === this.page) return item.title;
+    }
   }
 
 }
