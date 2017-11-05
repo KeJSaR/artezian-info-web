7881525ad20dab0dd82bc2f168deeebd784b87be
Constantine Verenich <verenich@gmail.com>
2017-11-05 20:26:42 +0300

    Remove MatSidenavModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,12 @@
 import { NgModule }         from '@angular/core';
 import { MatButtonModule }  from '@angular/material';
-import { MatSidenavModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    MatButtonModule,
-    MatSidenavModule
+    MatButtonModule
   ],
   exports: [
-    MatButtonModule,
-    MatSidenavModule
+    MatButtonModule
   ],
 })
 export class MaterialModule { }
