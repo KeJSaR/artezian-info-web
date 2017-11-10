22e21528882549887199a41ef1babd5fc7d42b96
Constantine Verenich <verenich@gmail.com>
2017-11-10 17:55:02 +0300

    Remove input page variable from blog component

--- src/app/parts/blog/blog.component.ts
+++ src/app/parts/blog/blog.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { Input }     from '@angular/core';
 
 @Component({
   selector    : 'app-blog',
   templateUrl : './blog.component.html',
   styleUrls   : ['./blog.component.sass']
 })
-export class BlogComponent {
-
-  @Input() page  : string;
-
-}
+export class BlogComponent { }
