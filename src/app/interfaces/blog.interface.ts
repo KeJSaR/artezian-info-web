8f39ae34b82f2a421431a4d41ceba4d742d2c4ea
Constantine Verenich <verenich@gmail.com>
2017-11-10 22:10:47 +0300

    Create blog interface

---
+++ src/app/interfaces/blog.interface.ts
@@ -0,0 +1,9 @@
+export interface Blog {
+  id           : number;
+  date         : string;
+  alias        : string;
+  title        : string;
+  author_name  : string;
+  author_title : string;
+  intro        : string;
+}
