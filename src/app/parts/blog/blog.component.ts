eb7a018808c2cf6bca495ec0e2987e6fc07b7de4
Constantine Verenich <verenich@gmail.com>
2017-11-10 22:11:05 +0300

    Add content and styles to blog component

--- src/app/parts/blog/blog.component.ts
+++ src/app/parts/blog/blog.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component }   from '@angular/core';
+import { OnInit }      from '@angular/core';
+
+import { DataService } from '../../services/data.service';
+import { Blog }        from '../../interfaces/blog.interface';
 
 @Component({
   selector    : 'app-blog',
   templateUrl : './blog.component.html',
   styleUrls   : ['./blog.component.sass']
 })
-export class BlogComponent { }
+export class BlogComponent implements OnInit {
+  
+  blog : Blog[];
+  
+  constructor(private dataService : DataService) { }
+
+  ngOnInit() {
+
+    this.dataService.getBlog().subscribe((data) => {
+      this.blog  = data;
+      console.log(data);
+    });
+
+  }
+  
+}
