6d8e50db42cdd68896c5019547b0385e9750175b
Constantine Verenich <verenich@gmail.com>
2017-11-10 16:38:21 +0300

    Add date and author data to article interface

--- src/app/interfaces/article.interface.ts
+++ src/app/interfaces/article.interface.ts
@@ -1,7 +1,10 @@
 export interface Article {
-  id      : number;
-  alias   : string;
-  title   : string;
-  intro   : string;
-  content : string;
+  id           : number;
+  date         : string;
+  alias        : string;
+  title        : string;
+  author_name  : string;
+  author_title : string;
+  intro        : string;
+  content      : string;
 }
