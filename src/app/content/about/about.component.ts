09888d59cebfbc48d0ecfdbc518eb13e25ac7e41
Constantine Verenich <verenich@gmail.com>
2017-11-13 20:04:31 +0300

    Refactor about component

--- src/app/content/about/about.component.ts
+++ src/app/content/about/about.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { DataService } from '../../services/data.service';
 import { Text } from '../../interfaces/text.interface';
-import { Intro } from '../../interfaces/intro.interface';
+import { BlogItem } from '../../interfaces/blog-item.interface';
 
 @Component({
   selector: 'app-about',
@@ -11,40 +11,38 @@ import { Intro } from '../../interfaces/intro.interface';
 })
 export class AboutComponent implements OnInit {
 
-  @Input() page: string;
+  @Input() alias: string;
 
   private texts: Text[];
-  private intros: Intro[];
+  private blogItems: BlogItem[];
   
-  isReady: boolean = false;
   about = [];
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
 
-    this.dataService.getText(this.page).subscribe((data) => {
+    this.dataService.getTexts(this.alias).subscribe((data) => {
       this.texts = data;
       this.makeAbout();
     });
 
-    this.dataService.getIntro(this.page).subscribe((data) => {
-      this.intros = data;
+    this.dataService.getBlogItems(this.alias).subscribe((data) => {
+      this.blogItems = data;
       this.makeAbout();
     });
 
   }
 
   private makeAbout(): void {
-    if (this.texts !== undefined && this.intros !== undefined) {
+    if (this.texts !== undefined && this.blogItems !== undefined) {
       let tL = this.texts.length;
-      let iL = this.intros.length;
+      let iL = this.blogItems.length;
       let max = tL > iL ? tL : iL;
       for (let i = 0; i < max; i++) {
         if (i < tL) this.about.push({type: 'text',  data: this.texts[i]});
-        if (i < iL) this.about.push({type: 'intro', data: this.intros[i]});
+        if (i < iL) this.about.push({type: 'intro', data: this.blogItems[i]});
       }
-      if (max > 0) this.isReady = true;
     }
   }
 
