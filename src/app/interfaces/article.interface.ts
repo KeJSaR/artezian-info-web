9ba452bc88fb2e12f9304532c943b3eb160381bb
Constantine Verenich <verenich@gmail.com>
2017-11-13 19:08:34 +0300

    Change property author as author info

--- src/app/interfaces/article.interface.ts
+++ src/app/interfaces/article.interface.ts
@@ -1,12 +1,11 @@
+import { AuthorInfo } from "./author-info.interface";
+
 export interface Article {
   id: number;
   date: string;
   alias: string;
   title: string;
-  author: {
-    name: string;
-    info: string;
-  };
+  author: AuthorInfo;
   intro: string;
   content: string;
 }
