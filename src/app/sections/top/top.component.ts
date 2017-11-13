e49fdfc233917bbfeb9174931f1996e590bf7a76
Constantine Verenich <verenich@gmail.com>
2017-11-13 18:39:59 +0300

    Add content to top component

--- src/app/sections/top/top.component.ts
+++ src/app/sections/top/top.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, HostListener, OnInit } from '@angular/core';
 
 import { DataService } from '../../services/data.service';
 import { Path } from '../../interfaces/path.interface';
+import { Gallery } from '../../interfaces/gallery.interface';
 
 @Component({
   selector: 'app-top',
@@ -13,15 +14,19 @@ export class TopComponent implements OnInit {
   @Input() section: Path;
   @Input() subsection: Path;
 
-  path: string = '';
-  image: string = '';
-  title: string = '';
-  name: string = '';
-  info: string = '';
-  intro: string = '';
-
+  gallery: Gallery;
   windowHeight: string;
   windowWidth: string;
+
+  // Main image data
+  path: string = '';
+  image: string = '';
+  // Page data
+  title: string = '';
+  intro: string = '';
+  // Author data
+  name: string = '';
+  info: string = '';
 
   constructor(private data: DataService) {
       this.windowHeight = this.setDimension(window.innerHeight);
@@ -29,23 +34,7 @@ export class TopComponent implements OnInit {
   }
 
   ngOnInit() {
-
-    if (this.subsection.alias) {
-      if (this.section.alias === 'gallery') {
-        this.path = 'gallery';
-        this.image = this.subsection.alias;
-      } else {
-        this.path = 'articles';
-        this.image = this.subsection.alias;
-      }
-    } else {
-      this.path = 'pages';
-      this.image = this.section.alias;
-    }
-
-    this.data.getMenuItems().subscribe((data) => {
-      this.menu = data;
-    });
+    this.init();
   }
 
   setDimension(num: number): string {
@@ -56,6 +45,39 @@ export class TopComponent implements OnInit {
   onResize(event) {
     this.windowHeight = this.setDimension(event.target.innerHeight);
     this.windowWidth  = this.setDimension(event.target.innerWidth);
+  }
+
+  private init(): void {
+    if (this.subsection.alias) {
+      if (this.section.alias === 'gallery') {
+        this.path = 'gallery';
+        this.setGalleryInfo(this.subsection.alias);
+      }
+      else {
+        this.path = 'articles';
+        this.image = this.subsection.alias;
+        this.title = this.subsection.name;
+      }
+    }
+    else {
+      this.path = 'pages';
+      this.image = this.section.alias;
+      this.title = this.section.name;
+    }
+  }
+
+  private setGalleryInfo(galleryId: string): void {
+    this.data.getGalleryInfo(galleryId).subscribe((data) => {
+      this.gallery = data;
+      this.setGalleryData();
+    });
+  }
+
+  private setGalleryData()
+  {
+    this.image = this.gallery.image.toString();
+    this.title = this.gallery.name;
+    this.intro = this.gallery.info;
   }
 
 }
