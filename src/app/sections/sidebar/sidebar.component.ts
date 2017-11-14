72228e41dfc65a8ef6cb364a35b78a0c53779280
Constantine Verenich <verenich@gmail.com>
2017-11-14 04:27:44 +0300

    Add sidebar component

---
+++ src/app/sections/sidebar/sidebar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { DataService } from '../../services/data.service';
+import { MenuItem } from '../../interfaces/menu-item.interface';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.sass']
+})
+export class SidebarComponent implements OnInit {
+
+  menu: MenuItem[];
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.setMenu();
+  }
+
+  private setMenu() {
+    this.data.getMenuItems().subscribe((data) => {
+      this.menu = data;
+    });
+  }
+
+}
