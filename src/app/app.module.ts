f9f016fbedbd9df091219b6d9bfce626f80919c5
Constantine Verenich <verenich@gmail.com>
2017-11-14 04:26:38 +0300

    Add sidebar conponent and show info directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,48 +6,50 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { DataService } from './services/data.service';
 
+import { ShowInfoDirective } from './directives/show-info.directive';
+
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
-
-import { HeaderComponent } from './sections/header/header.component';
-import { BreadcrumbsComponent } from './sections/breadcrumbs/breadcrumbs.component';
-import { FooterComponent } from './sections/footer/footer.component';
-import { CallComponent } from './sections/call/call.component';
-import { MenuComponent } from './sections/menu/menu.component';
-import { TopComponent } from './sections/top/top.component';
-
-import { AboutComponent } from './content/about/about.component';
-import { TextComponent } from './content/about/text/text.component';
 import { BlogItemComponent } from './content/about/blog-item/blog-item.component';
-
+import { TextComponent } from './content/about/text/text.component';
+import { AboutComponent } from './content/about/about.component';
+import { AlbumComponent } from './content/album/album.component';
 import { ArticleComponent } from './content/article/article.component';
 import { BlogComponent } from './content/blog/blog.component';
-import { NotFoundComponent } from './content/not-found/not-found.component';
-import { GalleryComponent } from './content/gallery/gallery.component';
-import { AlbumComponent } from './content/album/album.component';
 import { FormComponent } from './content/form/form.component';
+import { GalleryComponent } from './content/gallery/gallery.component';
 import { MainComponent } from './content/main/main.component';
+import { NotFoundComponent } from './content/not-found/not-found.component';
+import { BreadcrumbsComponent } from './sections/breadcrumbs/breadcrumbs.component';
+import { CallComponent } from './sections/call/call.component';
+import { FooterComponent } from './sections/footer/footer.component';
+import { HeaderComponent } from './sections/header/header.component';
+import { MenuComponent } from './sections/menu/menu.component';
+import { SidebarComponent } from './sections/sidebar/sidebar.component';
+import { TopComponent } from './sections/top/top.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageComponent,
-    HeaderComponent,
-    BreadcrumbsComponent,
-    FooterComponent,
-    TextComponent,
     BlogItemComponent,
+    TextComponent,
+    AboutComponent,
+    AlbumComponent,
     ArticleComponent,
     BlogComponent,
-    AboutComponent,
-    NotFoundComponent,
-    GalleryComponent,
-    AlbumComponent,
     FormComponent,
+    GalleryComponent,
     MainComponent,
+    NotFoundComponent,
+    BreadcrumbsComponent,
     CallComponent,
+    FooterComponent,
+    HeaderComponent,
     MenuComponent,
-    TopComponent
+    SidebarComponent,
+    TopComponent,
+    ShowInfoDirective
   ],
   imports: [
     BrowserModule,
