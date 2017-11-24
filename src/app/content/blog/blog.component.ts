64efb8d2f97376498cd1b4d24a3c3b8b2ccdad29
Constantine Verenich <verenich@gmail.com>
2017-11-24 23:04:17 +0300

    Add pages

--- src/app/content/blog/blog.component.ts
+++ src/app/content/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { DataService } from '../../services/data.service';
 import { BlogItem } from '../../interfaces/blog-item.interface';
@@ -13,15 +14,39 @@ export class BlogComponent implements OnInit {
   @Input() alias: string;
 
   blogItems: BlogItem[];
+  currentPage: number = 1;
+  lastPage: number;
 
-  constructor(private dataService: DataService) { }
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.setCurrentPage();
+    this.setLastPage();
     this.setBlogItems();
   }
 
+  private setCurrentPage(): void {
+    this.route.queryParamMap
+              .map((params: Params) => params.params)
+              .subscribe((params) => {
+                if (params && params['page']) {
+                  this.currentPage = Number(params['page']);
+                }
+              });
+  }
+
+  private setLastPage(): void {
+    this.dataService.getLastPage(this.alias).subscribe((data) => {
+      this.lastPage = data;
+    });
+  }
+
   private setBlogItems(): void {
-    this.dataService.getBlogItems(this.alias).subscribe((data) => {
+    let page = this.currentPage.toString();
+    this.dataService.getBlogItems(this.alias, page).subscribe((data) => {
       this.blogItems = data;
     });
   }
