8b989e0e6fd410b2debcb8a530c17179d7ecec7e
Constantine Verenich <verenich@gmail.com>
2017-11-24 17:54:55 +0300

    Move texts to main component

--- src/app/content/about/about.component.ts
+++ src/app/content/about/about.component.ts
@@ -13,43 +13,18 @@ export class AboutComponent implements OnInit {
 
   @Input() alias: string;
 
-  private texts: Text[];
   private blogItems: BlogItem[];
-
-  about = [];
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.setTexts();
     this.setBlogItems();
-  }
-
-  private setTexts(): void {
-    this.dataService.getTexts(this.alias).subscribe((data) => {
-      this.texts = data;
-      this.makeAbout();
-    });
   }
 
   private setBlogItems(): void {
     this.dataService.getBlogItems(this.alias).subscribe((data) => {
       this.blogItems = data;
-      this.makeAbout();
     });
-  }
-
-  private makeAbout(): void {
-    if (this.texts !== undefined && this.blogItems !== undefined) {
-      let tL = this.texts.length;
-      let iL = this.blogItems.length;
-      let max = tL > iL ? tL : iL;
-      let t = 0;
-      for (let i = 0; i < max; i++) {
-        if (t < tL && i % 2 === 0) this.about.push({type: 'text',  data: this.texts[t++]});
-        if (i < iL) this.about.push({type: 'intro', data: this.blogItems[i]});
-      }
-    }
   }
 
 }
--- src/app/content/main/main.component.ts
+++ src/app/content/main/main.component.ts
@@ -1,20 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener, OnInit } from '@angular/core';
 
+import { DataService } from '../../services/data.service';
 import { MenuItem } from '../../interfaces/menu-item.interface';
+import { Text } from '../../interfaces/text.interface';
 
 @Component({
   selector: 'aae-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.sass']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
   @Input() menu: MenuItem[];
 
+  texts: Text[];
   height: string;
 
-  constructor() {
-    this.height = window.innerHeight.toString() + 'px';
+  constructor(private dataService: DataService) {
+    this.height = this.setDimension(window.innerHeight);
+  }
+
+  ngOnInit() {
+    this.setTexts();
+  }
+
+  setDimension(num: number): string {
+    return num.toString() + 'px';
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.height = this.setDimension(event.target.innerHeight);
+  }
+
+  private setTexts(): void {
+    this.dataService.getTexts('about').subscribe((data) => {
+      this.texts = data;
+    });
   }
 
 }
