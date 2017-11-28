3c11469589cc081f0f03085432e057603b3e7d59
Constantine Verenich <verenich@gmail.com>
2017-11-28 03:00:16 +0300

    Edit main view

--- src/app/content/main/main.component.ts
+++ src/app/content/main/main.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, HostListener, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { MenuItem } from '../../interfaces/menu-item.interface';
 import { Text } from '../../interfaces/text.interface';
+import { TextMain } from '../../interfaces/text-main.interface';
 
 @Component({
   selector: 'aae-main',
@@ -13,29 +14,45 @@ export class MainComponent implements OnInit {
 
   @Input() menu: MenuItem[];
 
-  texts: Text[];
+  intro: string;
+  texts: TextMain[] = [];
   height: string;
+  contentHeight: string;
 
   constructor(private dataService: DataService) {
-    this.height = this.setDimension(window.innerHeight);
+    this.setDimension(window.innerHeight);
   }
 
   ngOnInit() {
     this.setTexts();
   }
 
-  setDimension(num: number): string {
-    return num.toString() + 'px';
+  setDimension(num: number): void {
+    this.height = num.toString() + 'px';
+    this.contentHeight = (num - 300).toString() + 'px';
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-    this.height = this.setDimension(event.target.innerHeight);
+    this.setDimension(event.target.innerHeight);
   }
 
   private setTexts(): void {
     this.dataService.getTexts('about').subscribe((data) => {
-      this.texts = data;
+      this.prepareTexts(data);
+    });
+  }
+
+  private prepareTexts(texts: Text[]) {
+    this.intro = texts[0].content;
+    texts.forEach((text) => {
+      if (text.id > 1) {
+        this.texts.push({
+          img: text.id,
+          content: text.content,
+          type: text.id % 2 === 0 ? 'left' : 'right'
+        });
+      }
     });
   }
 
