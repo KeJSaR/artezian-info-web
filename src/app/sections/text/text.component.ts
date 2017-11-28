744141baf5cf1bb4075b0ee9a0bd44e749a6ab5f
Constantine Verenich <verenich@gmail.com>
2017-11-28 03:00:03 +0300

    Remove extra height property

--- src/app/sections/text/text.component.ts
+++ src/app/sections/text/text.component.ts
@@ -1,33 +1,14 @@
-import { Component, Input, OnInit, HostListener } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import { Text } from '../../interfaces/text.interface';
+import { TextMain } from '../../interfaces/text-main.interface';
 
 @Component({
   selector: 'aae-text',
   templateUrl: './text.component.html',
   styleUrls: ['./text.component.sass']
 })
-export class TextComponent implements OnInit {
+export class TextComponent {
 
-  @Input() text: Text;
-
-  height: string
-
-  ngOnInit() {
-    this.setHeight();
-  }
-
-  private setHeight(): void {
-    this.height = this.setDimension(window.innerHeight);
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.height = this.setDimension(event.target.innerHeight);
-  }
-
-  setDimension(num: number): string {
-    return (num - 520).toString() + 'px';
-  }
+  @Input() text: TextMain;
 
 }
