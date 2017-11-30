d2e823327068af896a65b9031bc2b9291582d68a
Constantine Verenich <verenich@gmail.com>
2017-11-30 22:48:37 +0300

    Change selector

--- src/app/content/contact/contact.component.ts
+++ src/app/content/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contact',
+  selector: 'aae-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.sass']
 })
--- src/app/sections/map/map.component.ts
+++ src/app/sections/map/map.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-map',
+  selector: 'aae-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.sass']
 })
